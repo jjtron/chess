b15e841b6e9763dd9988f0b7376bca7f202e6a91
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-06-15 19:00:35 -0400

    Add ANALYTICAL comments about how this works

--- app/ui/Draggable.jsx
+++ app/ui/Draggable.jsx
@@ -7,8 +7,26 @@ export function Draggable(props) {
   const {attributes, listeners, setNodeRef, transform} = useDraggable({
     id: props.id,
   });
+  {/*
+    the attributes are
+      {
+        "role": "button",
+        "tabIndex": 0,
+        "aria-disabled": false,
+        "aria-roledescription": "draggable",
+        "aria-describedby": "DndDescribedBy-0"
+      }
+    the listeners are
+      {
+        onPointerDown: ƒunction,
+        onKeyDown: ƒunction
+      }
+  */}
   const style = {
     // Outputs `translate3d(x, y, 0)`
+    /* How come not as follows ... ?
+          transform: CSS.Translate.toString({transform})
+    */
     transform: CSS.Translate.toString(transform),
   };
 
--- app/ui/Example.jsx
+++ app/ui/Example.jsx
@@ -14,14 +14,33 @@ export default function Example() {
 
   return (
     <DndContext onDragEnd={handleDragEnd}>
+      {/* if the 'parent' var is NULL (!parent === true),
+          then the Draggable component exists outside of the Droppable component,
+          i.e. the following expression yields <Draggable . . . />;
+      */}
+
       {!parent ? draggable : null}
+
+      {/* but if the 'parent' var is NOT NULL,
+          then the Draggable component does NOT exist outside of the Droppable component,
+          but rather . . .
+      */}
       <Droppable id="droppable">
+        {/* . . . the parent var, being equal to 'droppable',
+            makes the Draggable component exist inside the Droppable component,
+            i.e. the following expression yields <Draggable . . . />;
+        */}
         {parent === "droppable" ? draggable : 'Drop here'}
       </Droppable>
     </DndContext>
   );
 
   function handleDragEnd({over}) {
+    {/*  if Draggable is dropped outside of the Droppable component,
+         then over = null.
+         But if Draggable is dropped inside the Droppable component,
+         then over = {id: 'droppable', rect: Rect, data: {…}, disabled: false}
+    */}
     setParent(over ? over.id : null);
   }
 }
