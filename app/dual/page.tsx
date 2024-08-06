3e1cb7d68b0067f04e445d6d82c85bf8b360379c
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-08-06 18:51:01 -0400

    Center dual player set-up html

--- app/dual/page.tsx
+++ app/dual/page.tsx
@@ -53,25 +53,33 @@ export default function Dnd() {
     return <Board adversaryID={adversaryID} registrationID={registrationID} isOpponentSelf={true} />
   } else {
     return (
-      <div className='p-2 min-h-screen'>
+      <div className='p-2 min-h-screen flex flex-row'>
+        <div className='basis-1/3'></div>
+        <div className='min-w-[320px] sm:min-w-[420px]'>
         <div className='p-2'>
-          <p>Your registrationID is
+          <p>Your registration ID is
             <span className={clsx('pl-2', {'hidden' : !!registrationID })} >. . .</span>
             <span className={clsx('pl-2', {'hidden' : !registrationID })} >:</span>
             <span className='pl-1 text-lime-500'>&nbsp;{registrationID}</span>
           </p>
+          <p className={clsx({'hidden' : !registrationID })}>E-mail or text it to your opoonent.</p>
         </div>
         <div className={clsx('p-2', {'hidden' : !registrationID })}>
-          <p>Paste Opponent&apos;s Registration ID here, and click Handshake to get started.</p>
+          <p>Paste your opponent&apos;s registration ID here
+            <span className={clsx('pl-2', {'hidden' : !!adversaryID })} >. . .</span>
+          </p>
           <input type='text' size={25} value={adversaryID} onChange={handleSetOpponent} className='px-1 text-black' />
         </div>
-        <div className={clsx('flex flex-row p-2', {'hidden' : !registrationID })}>
-          <button onClick={handleHandshake}
-                  disabled={!adversaryID}
-                  className={clsx('px-2 bg-slate-300 border border-white rounded-md text-black',
-                            { 'text-black' :  !!adversaryID, 'text-slate-600' : !adversaryID } )}>Handshake
-          </button>
-          <p className={clsx('px-2', {'hidden' : !messageResponse })}>{messageResponse}</p>
+        <div>
+          <p className={clsx('pl-2', {'hidden' : !adversaryID })} >Click Handshake to get started.</p>
+          <div className={clsx('flex flex-row pl-2', {'hidden' : !registrationID })}>
+            <button onClick={handleHandshake}
+                    disabled={!adversaryID}
+                    className={clsx('px-2 bg-slate-300 border border-white rounded-md text-black',
+                              { 'hidden' : !adversaryID })}>Handshake
+            </button>
+            <p className={clsx('px-2', {'hidden' : !messageResponse })}>{messageResponse}</p>
+          </div>
         </div>
         <div className={clsx('p-2', {'hidden' : !message })}>
           <p>Handshake message from opponent with id: {message}</p>
@@ -82,6 +90,8 @@ export default function Dnd() {
                             { 'text-black' :  !!adversaryID, 'text-slate-600' : !adversaryID } )}>Play
           </button>
         </div>
+        </div>
+        <div className='basis-1/3'></div>
       </div>
     );
   }
